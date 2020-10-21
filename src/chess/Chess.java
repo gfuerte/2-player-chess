6587d6475372e011428b407c024ded5ae744d583
gfuerte <gregorio.fuerte@gmail.com>
2020-10-21 01:23:09 -0400

    initial commit

---
+++ src/chess/Chess.java
@@ -0,0 +1,11 @@
+package chess;
+
+public class Chess {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("i suck at chess");
+		System.out.println("chess sucks at i");
+	}
+
+}
